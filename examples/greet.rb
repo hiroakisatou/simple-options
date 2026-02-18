9ce037c70bdc85a11e30bb5545f26461edc4cc4c
hiroakisatou <hiroakisatou@protonmail.com>
2026-02-18 16:20:47 +0900

    update example not use ARGV

--- examples/greet.rb
+++ examples/greet.rb
@@ -6,11 +6,11 @@ require_relative '../lib/option'
 opts = Options.new(description: 'Print a greeting.')
 opts.add Option.new(:name, short: '-n', long: '--name', desc: 'Name to greet', required: true)
 opts.add Option.new(:count, short: '-c', long: '--count', desc: 'Repeat count')
-       .validate { |v| v.to_i.positive? }
-       .convert(&:to_i)
+               .validate { |v| v.to_i.positive? }
+               .convert(&:to_i)
 
 if __FILE__ == $0 && !ARGV.empty?
-  opts.parse!(ARGV)
+  opts.parse!
   msg = "Hello, #{opts.get(:name)}!"
   count = opts.get(:count) || 1
   count.times { puts msg }
--- examples/paint_calculator.rb
+++ examples/paint_calculator.rb
@@ -13,7 +13,7 @@ opts.add Option.new(:width, desc: 'Width of the room', short: '-w', long: '--wid
                .convert(&:to_f)
 
 if __FILE__ == $0 && !ARGV.empty?
-  opts.parse!(ARGV)
+  opts.parse!
   area = opts.get(:length) * opts.get(:width)
   gallons = (area / 350.0).ceil
   puts "You will need to purchase #{gallons} gallons to cover #{area} square feet."
